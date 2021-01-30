38aacc947834b35aec7184022dce5b5cc3878fe8
fernanduandrade <nando.jua@gmail.com>
2021-01-30 14:57:03 -0300

    add backend app

---
+++ backend/classified/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Classified(models.Models):
+
+	title = models.CharField(max_length=50, blank=False)
+	description = models.CharField(max_length=255, blank=False)
+	date = models.DateField(auto_now_add=True)
